8ac767a876aac96ccab6c7b2399754c9e26e11d3
Mahesh Dabhi <maheshdabhi@Maheshs-Mac-mini.local>
2024-10-05 14:47:22 +0530

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:ecommerce_seller/presentation/on_boarding_section/splash_screen/splash_screen.dart';
+import 'package:ecommerce_seller/utilz/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveSizer(builder: (context, orientation, deviceType) {
+      return GetMaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: backGroundColor,
+          appBarTheme: const AppBarTheme()
+              .copyWith(color: Colors.white, surfaceTintColor: Colors.white),
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: SplashScreen(),
+      );
+    });
+  }
+}
